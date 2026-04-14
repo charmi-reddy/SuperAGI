772f62c116875f45152022717202bd89efc98ef7
Charmi Reddy P <23ra1a6620@kpritech.ac.in>
2026-04-14 16:15:58 +0000

    fix(model-info): guard against undefined model details during render.

--- gui/pages/Content/Models/ModelInfo.js
+++ gui/pages/Content/Models/ModelInfo.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import Image from "next/image";
 
-export default function ModelInfo(modelDetails){
-    const [modelData, setModelData] = useState(modelDetails?.modelDetails)
+export default function ModelInfo({modelDetails}){
+    const modelData = modelDetails || {};
+
     return(
         <div id="model_info" className="text_12">
             <hr className="horizontal_line padding_0" />
@@ -11,12 +12,12 @@ export default function ModelInfo(modelDetails){
                 <div className="col-6 col-6-scrollable vertical_containers">
                     <span>Installation Type</span>
                     <div className="horizontal_container mt_8 color_white gap_4">
-                        {modelData === 'Marketplace' && <Image width={16} height={16} src="/images/marketplace_logo.png" alt="marketplace_logo" />}
-                        <span>{modelData.type}</span>
+                        {modelData.type === 'Marketplace' && <Image width={16} height={16} src="/images/marketplace_logo.png" alt="marketplace_logo" />}
+                        <span>{modelData.type || '-'}</span>
                     </div>
 
                     <span className="mt_24">Model Provider</span>
-                    <span className="mt_8 color_white">{modelData.model_provider}</span>
+                    <span className="mt_8 color_white">{modelData.model_provider || '-'}</span>
 
                     {modelData.end_point && <div className="vertical_containers">
                         <span className="mt_24">Model Endpoint</span>
@@ -24,7 +25,7 @@ export default function ModelInfo(modelDetails){
                     </div>}
 
                     <span className="mt_24">Token Limit</span>
-                    <input className="input_medium mt_8" type="number" placeholder="Enter Model Token Limit" value={modelData.token_limit} disabled/>
+                    <input className="input_medium mt_8" type="number" placeholder="Enter Model Token Limit" value={modelData.token_limit || ''} disabled/>
                 </div>
                 <div className="col-3" />
             </div>
