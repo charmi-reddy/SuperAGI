26c971ae99c04aba04c173f1ae6bbb2c2196a85e
Charmi Reddy P <23ra1a6620@kpritech.ac.in>
2026-04-21 20:44:21 +0530

    Replace hardcoded timeout with MARKETPLACE_TIMEOUT

--- superagi/models/vector_dbs.py
+++ superagi/models/vector_dbs.py
@@ -8,6 +8,7 @@ from superagi.models.base_model import DBBaseModel
 
 marketplace_url = "https://app.superagi.com/api"
 # marketplace_url = "http://localhost:8001"
+MARKETPLACE_TIMEOUT = 10
 
 class Vectordbs(DBBaseModel):
     """
@@ -44,7 +45,7 @@ class Vectordbs(DBBaseModel):
         headers = {'Content-Type': 'application/json'}
         response = requests.get(
             marketplace_url + f"/vector_dbs/marketplace/list",
-            headers=headers, timeout=10)
+            headers=headers, timeout=MARKETPLACE_TIMEOUT)
         if response.status_code == 200:
             return response.json()
         else:
