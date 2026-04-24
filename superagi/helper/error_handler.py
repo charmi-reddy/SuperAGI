2dc4c99e92d39015e10a63e1691543c1d310c47f
Charmi Reddy P <23ra1a6620@kpritech.ac.in>
2026-04-24 22:07:02 +0530

    Fix potential NoneType error in error handler

--- superagi/helper/error_handler.py
+++ superagi/helper/error_handler.py
@@ -5,6 +5,7 @@ class ErrorHandler:
 
     def handle_openai_errors(session, agent_id, agent_execution_id, error_message):
         execution = session.query(AgentExecution).filter(AgentExecution.id == agent_execution_id).first()
-        agent_feed = AgentExecutionFeed(agent_execution_id=agent_execution_id, agent_id=agent_id, role="system", feed="", error_message=error_message, feed_group_id=execution.current_feed_group_id)
+        feed_group_id = execution.current_feed_group_id if execution is not None else None
+        agent_feed = AgentExecutionFeed(agent_execution_id=agent_execution_id, agent_id=agent_id, role="system", feed="", error_message=error_message, feed_group_id=feed_group_id)
         session.add(agent_feed)
         session.commit()
