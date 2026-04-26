a2e046b8676737c4dc4e59c0bb191e28512e6b65
Charmi Reddy P <23ra1a6620@kpritech.ac.in>
2026-04-26 21:01:51 +0530

    Update error messages in get_models_types method

--- superagi/controllers/types/models_types.py
+++ superagi/controllers/types/models_types.py
@@ -7,8 +7,8 @@ class ModelsTypes(Enum):
     @classmethod
     def get_models_types(cls, model_type):
         if model_type is None:
-            raise ValueError("Queue status type cannot be None.")
-        model_type = model_type.upper()
+            raise ValueError("Model type cannot be None.")
+        model_type = str(model_type).upper().strip()
         if model_type in cls.__members__:
             return cls[model_type]
-        raise ValueError(f"{model_type} is not a valid storage name.")
+        raise ValueError(f"{model_type} is not a valid model type name.")
